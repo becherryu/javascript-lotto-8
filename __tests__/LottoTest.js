183fc8cedc74d9d1ac3b3e1e3b0a9e4b11e804f5
becherryu <81617058+becherryu@users.noreply.github.com>
2025-11-03 23:56:38 +0900

    refactor(LottoTest.js): 추가 테스트 코드 작성

--- __tests__/LottoTest.js
+++ __tests__/LottoTest.js
@@ -7,12 +7,26 @@ describe("로또 클래스 테스트", () => {
     }).toThrow("[ERROR]");
   });
 
-  // TODO: 테스트가 통과하도록 프로덕션 코드 구현
   test("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.", () => {
     expect(() => {
       new Lotto([1, 2, 3, 4, 5, 5]);
     }).toThrow("[ERROR]");
   });
 
-  // TODO: 추가 기능 구현에 따른 테스트 코드 작성
+  test("로또 번호가 6개 미만이면 예외가 발생한다.", () => {
+    expect(() => new Lotto([1, 2, 3, 4, 5])).toThrow("[ERROR]");
+  });
+
+  test("로또 번호가 숫자가 아닐 경우 예외가 발생한다.", () => {
+    expect(() => new Lotto(["a", 2, 3, 4, 5, 6])).toThrow("[ERROR]");
+  });
+
+  test("로또 번호가 1 미만이거나 45 초과인 경우 예외가 발생한다.", () => {
+    expect(() => new Lotto([0, 2, 3, 4, 5, 6])).toThrow("[ERROR]");
+    expect(() => new Lotto([1, 2, 3, 4, 5, 46])).toThrow("[ERROR]");
+  });
+
+  test("로또 번호가 6개이고 중복 없이 1~45 범위의 숫자면 정상 생성된다.", () => {
+    expect(() => new Lotto([1, 2, 3, 4, 5, 6])).not.toThrow();
+  });
 });
