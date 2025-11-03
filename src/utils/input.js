b309ea480ff4343c5b6d2ac2fe899ad01c5acd3f
becherryu <81617058+becherryu@users.noreply.github.com>
2025-11-03 23:09:00 +0900

    fix(input.js): 출력 형식 변경

--- src/utils/input.js
+++ src/utils/input.js
@@ -5,9 +5,9 @@ export async function getPurchasePrice() {
 }
 
 export async function getLottoNumbers() {
-  return Console.readLineAsync("당첨 번호를 입력해 주세요.\n");
+  return Console.readLineAsync("\n당첨 번호를 입력해 주세요.\n");
 }
 
 export async function getBonusNumber() {
-  return Console.readLineAsync("보너스 번호를 입력해 주세요.\n");
+  return Console.readLineAsync("\n보너스 번호를 입력해 주세요.\n");
 }
